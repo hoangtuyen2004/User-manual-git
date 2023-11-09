aee7da4f90955e47e4a5377264b034c3a58d4406
hoangtuyen2004 <ductuyen772@gmail.com>
2023-11-09 18:15:52 +0700

    update index.php file

--- index.php
+++ index.php
@@ -7,6 +7,29 @@
     <link rel="icon" href="https://i.pinimg.com/736x/1c/9c/79/1c9c798c5dc8b2134ffc22fabfc7bedb.jpg">
 </head>
 <body>
-    <h1>A hi hi</h1>
+    <header>
+        <div class="img-us">
+            <img class="img-logo" src="https://i.pinimg.com/564x/9e/ef/a6/9eefa69f33cda9ece362a920f323ec0a.jpg" alt="hình ảnh">
+        </div>
+
+
+        <p><span class="git">git</span> <span class="action">pull</span></p>
+        <!-- git pull: đồng bộ dữ liệu từ kho lưu trữ git về máy -->
+
+
+
+        <p><span class="git">git</span> <span class="action">log</span></p>
+        <!-- git log: kiểm tra lại lịch sử commit trên git -->
+
+
+
+        <p><span class="git">git</span> <span class="action">status</span></p>
+        <!-- git status: cho biết các thông số bao gồm: 
+                                +tệp đã được thay đổi,
+                                +tệp được thêm mới ,
+                                +tệp đã được xóa,
+                                +tệp chưa được theo dõi -->
+                            
+    </header>
 </body>
 </html>
